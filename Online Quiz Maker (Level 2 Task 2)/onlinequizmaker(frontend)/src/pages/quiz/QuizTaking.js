ddeea33a7248fbb21730e51856ee52a877f18762
kirubel <kirato747@gmail.com>
2023-10-28 21:37:10 +0300

    Integrate quiz score update API

--- Online Quiz Maker (Level 2 Task 2)/onlinequizmaker(frontend)/src/pages/quiz/QuizTaking.js
+++ Online Quiz Maker (Level 2 Task 2)/onlinequizmaker(frontend)/src/pages/quiz/QuizTaking.js
@@ -1,196 +1,202 @@
-import React,{useState} from 'react'
-import { useLocation } from 'react-router-dom';
-import QuizResult from './QuizResult';
-import axios from 'axios';
-import api from '../../utils/api';
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import QuizResult from "./QuizResult";
+import axios from "axios";
+import api from "../../utils/api";
+
+import Button from "../../components/common/Button";
+
 const QuizTaking = () => {
-  const[quiz,setQuiz]=useState([])
-  const[color,setColor]=useState(false)
-  const[totalScore,setTotalScore]=useState(0)
-    const[isLoading,setIsLoading]=useState(true)
-        
-          const[alphabet]=useState( ["A","B","C","D"])
+  const [quiz, setQuiz] = useState([]);
+  const [totalScore, setTotalScore] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+  const width = window.innerWidth;
+
+  const height = window.innerHeight;
+  const [timeElapsed, setTimeElapsed] = useState(0); // State to track time in seconds
   const location = useLocation();
-           const id=location.pathname.split("/")[2]
-// console.log()
-    // const QuizList={
-    //   "quizzes": [
-    //     {
-    //       "id": 1,
-    //       "category": "Science",
-    //       "title": "Chemical Elements",
-    //       "description": "Test your knowledge of chemical elements and their symbols.",
-    //       "author": "John Doe",
-    //       "questions": [
-    //         {
-    //           "id": 1,
-    //           "questionText": "What is the chemical symbol for water?",
-    //           "options": ["H2O", "CO2", "O2", "N2"],
-    //           "correctAnswer": "H2O"
-    //         },
-    //         {
-    //           "id": 2,
-    //           "questionText": "What is the chemical symbol for helium?",
-    //           "options": ["He", "Hl", "H", "H2"],
-    //           "correctAnswer": "He"
-    //         },
-    //         {
-    //           "id": 3,
-    //           "questionText": "Which element is known as the 'noble gas'?",
-    //           "options": ["Neon", "Sodium", "Argon", "Chlorine"],
-    //           "correctAnswer": "Argon"
-    //         }
-            
-            
-    //       ]
-    //     },
-    //     {
-    //       "id": 2,
-    //       "category": "Maths",
-    //       "title": "Algebra Basics",
-    //       "description": "Test your algebra skills with this introductory quiz.",
-    //       "author": "Jane Smith",
-    //       "questions": [
-    //         {
-    //           "id": 2,
-    //           "questionText": "What is 5 + 7?",
-    //           "options": ["10", "12", "8", "14"],
-    //           "correctAnswer": "12"
-    //         },
-    //         // Add more questions for this quiz
-    //       ]
-    //     },
-    //     {
-    //       "id": 3,
-    //       "category": "Chemistry",
-    //       "title": "Chemical Reactions",
-    //       "description": "Explore chemical reactions and their principles.",
-    //       "author": "Alice Johnson",
-    //       "questions": [
-    //         {
-    //           "id": 3,
-    //           "questionText": "What is the atomic number of oxygen?",
-    //           "options": ["6", "8", "16", "22"],
-    //           "correctAnswer": "8"
-    //         },
-    //         // Add more questions for this quiz
-    //       ]
-    //     }
-    //     // Add more quizzes here
-    //   ]
-    // }
-// const[answerItems,setAnswerItems]=useState([])
-   
-        //   const handleClick=(a,b)=>{
+  const id = location.pathname.split("/")[2];
+  const navigate = useNavigate();
+  const [selectedAnswers, setSelectedAnswers] = useState({});
+  const handleAnswerSelection = (questionId, selectedAnswer) => {
+    setSelectedAnswers((prevSelectedAnswers) => ({
+      ...prevSelectedAnswers,
+      [questionId]: selectedAnswer,
+    }));
+  };
+  const [showModal, setShowModal] = useState(false);
 
-        // //  if(a===b){
-        // //    setTotalScore(totalScore+1);
-        // //   //  alert("you picked the correct answer")
-        // //  }
+  const openModal = () => {
+    setShowModal(true);
+  };
 
-        //   }
-        const [selectedAnswers, setSelectedAnswers] = useState({});
-          const handleAnswerSelection = (questionId, selectedAnswer) => {
-            setSelectedAnswers((prevSelectedAnswers) => ({
-              ...prevSelectedAnswers,
-              [questionId]: selectedAnswer,
-            }));
-          };
-          console.log(selectedAnswers,"Selected answers")
-   
-          const [showModal, setShowModal] = useState(false);
-
-          const openModal = () => {
-            setShowModal(true);
-          };
-        
-          const closeModal = () => {
-            setShowModal(false);
-          };
-  React.useEffect(()=>{
-    const cancelToken=axios.CancelToken.source()
-    api.get(`/quiz/${id}`,{cancelToken:cancelToken.token}).then((res)=>{
-      setQuiz(res.data)
-      setIsLoading(false)
-    }).catch((err)=>{
-        if(axios.isCancel(err)){
-          console.log("Request canceled")
-        }else{
-
+  const closeModal = () => {
+    setShowModal(false);
+  };
+  React.useEffect(() => {
+    const cancelToken = axios.CancelToken.source();
+    api
+      .get(`/quiz/${id}`, { cancelToken: cancelToken.token })
+      .then((res) => {
+        setQuiz(res.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        if (axios.isCancel(err)) {
+          console.log("Request canceled");
+        } else {
         }
-    })
-    return()=>{
-      cancelToken.cancel()
-    }
-  },[])
+      });
+    return () => {
+      cancelToken.cancel();
+    };
+  }, []);
   React.useEffect(() => {
     let score = 0;
-    let arrOfQuestions=quiz.questions
-    if(!isLoading){
-       console.log(Array.isArray(arrOfQuestions))
-   arrOfQuestions.map((data)=>{
-    const selectedAnswer=selectedAnswers[data._id]
-   if(selectedAnswer===data.correctAnswer){
-    score++;
-   }
-   })
+    let arrOfQuestions = quiz.questions;
+    if (!isLoading) {
+      arrOfQuestions.map((data) => {
+        const selectedAnswer = selectedAnswers[data._id];
+        if (selectedAnswer === data.correctAnswer) {
+          score++;
+        }
+      });
     }
-  //  quiz.questions.map((quiz)=>{
-  
-  //  })
-   
-
-      // quiz.questions.forEach((question) => {
-      //   const selectedAnswer = selectedAnswers[question._id];
-      //   if (selectedAnswer === question.correctAnswer) {
-      //     score++;
-      //   }
-      // });
 
     setTotalScore(score);
-  }, [selectedAnswers, quiz,isLoading]);
+  }, [selectedAnswers, quiz, isLoading]);
   // console.log(quiz)
+  useEffect(() => {
+    const timerInterval = setInterval(() => {
+      setTimeElapsed((prevTime) => prevTime + 1); // Increment the time elapsed by 1 second
+    }, 1000); // Update every second
+
+    return () => {
+      clearInterval(timerInterval); // Clean up the interval when the component unmounts
+    };
+  }, []);
+
+  const formatTime = (seconds) => {
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const remainingSeconds = seconds % 60;
+    const formattedHours = hours.toString().padStart(2, "0");
+    const formattedMinutes = minutes.toString().padStart(2, "0");
+    const formattedSeconds = remainingSeconds.toString().padStart(2, "0");
+
+    if (hours > 0) {
+      return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+    } else if (minutes > 0) {
+      return `${formattedMinutes}:${formattedSeconds}`;
+    } else {
+      return `0:${formattedSeconds}`;
+    }
+  };
+  const timeDisplay = formatTime(timeElapsed);
+  const handleRetake = (e) => {
+    e.preventDefault();
+    setShowModal(false);
+    setSelectedAnswers({});
+  };
+  const userID = localStorage.getItem("userID");
+  const handleSend = (cancelToken) => {
+    api.patch(
+      `/quiz/updatescore/${id}`,
+      { userId: userID, score: totalScore },
+      cancelToken,
+    );
+  };
+  useEffect(() => {
+    const cancelToken = axios.CancelToken.source();
+    try {
+      if (showModal) {
+        handleSend({ cancelToken: cancelToken.token });
+      }
+    } catch (err) {
+      if (axios.isCancel(err)) {
+        console.log("Request canceled");
+      } else {
+        //Canceled
+      }
+    }
+    return () => {
+      console.log("useEffect unmounts");
+      cancelToken.cancel();
+    };
+  }, [showModal]);
   return (
-    <div className='flex flex-col justify-center items-center px-10 pt-16'>
-      <div className='flex flex-col justify-start items-start self'>
-          <p className='text-xl font-bold'>{totalScore}</p>
-          {/* <p>Current route: {location.pathname}</p> */}
-      {isLoading?<p>Loading...</p>:(
-       
-        <div className='boder-2 boder-red-600'>
-{
-  quiz.questions.map((questionItems,index)=>(
-  <div className='py-3 flex flex-col'>
-    <p className='text-xl font-medium'>{index+1}. {questionItems.questionText}</p>
-    {questionItems.options.map((option,index)=>(
-      <label className='p-2' htmlFor='choice'>
-<input type="radio" className='mr-2' id="choice" name={`question-${questionItems._id}`}
- onChange={()=>handleAnswerSelection(questionItems._id,option)} checked={selectedAnswers[questionItems._id]===option}/>
-{option}
-      </label>
-      // 
-    ))}
-  </div>
-  ))
-}
-        </div>
-      )}
-     
-      <button onClick={openModal} className=' text-white px-4 py-2 rounded-lg bg-[#996CF1]'>Submit</button>
-      {showModal && (<div className="fixed inset-0 flex items-center justify-center z-50">
-          <div className="bg-white w-[30vw] drop-shadow-lg shadow shadow-gray-400 rounded-lg p-10">
-            <div className="modal-content">
-              {/* Modal content goes here */}
-              <h2 className="text-2xl font-bold">Result</h2>
-              <QuizResult correct={totalScore} incorrect={quiz.questions.length-totalScore}/>
-              <button onClick={closeModal} className="bg-blue-500 text-white p-2 rounded mt-4">Close Modal</button>
+    <div className="flex flex-col justify-center items-center px-10 pt-16">
+      <div className="flex flex-col justify-start items-start self">
+        <p className="text-xl font-bold">{totalScore}</p>
+        <p className="text-sm text-gray-500">Time Elapsed: {timeDisplay}</p>
+        {/* <p>Current route: {location.pathname}</p> */}
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          <div className="boder-2 boder-red-600">
+            {quiz.questions.map((questionItems, index) => (
+              <div key={questionItems._id} className="py-3 flex flex-col">
+                <p className="text-xl font-medium">
+                  {index + 1}. {questionItems.questionText}
+                </p>
+                {questionItems.options.map((option, index) => (
+                  <div className="flex flex-col" key={questionItems._id}>
+                    <label className="p-2" htmlFor="choice">
+                      <input
+                        type="radio"
+                        className="mr-2"
+                        id="choice"
+                        name={`question-${questionItems._id}`}
+                        onChange={() =>
+                          handleAnswerSelection(questionItems._id, option)
+                        }
+                        checked={selectedAnswers[questionItems._id] === option}
+                      />
+                      {option}
+                    </label>
+                  </div>
+                  //
+                ))}
+              </div>
+            ))}
+          </div>
+        )}
+
+        <button
+          onClick={openModal}
+          className=" text-white px-4 py-2 rounded-lg bg-[#996CF1]"
+        >
+          Submit
+        </button>
+        {showModal && (
+          <div className="fixed inset-0 flex items-center justify-center z-50">
+            <div className="bg-white flex items-center justify-center drop-shadow-lg shadow shadow-gray-400 rounded-lg py-10 px-32">
+              <div className="modal-content flex flex-col items-center justify-center">
+                <h2 className="md:text-2xl text-xl font-bold">Result</h2>
+                <QuizResult
+                  correct={totalScore}
+                  incorrect={quiz.questions.length - totalScore}
+                />
+
+                <div className="flex flex-row w-full justify-around">
+                  <Button
+                    text="Retake Quiz"
+                    style="bg-[#996CF1] font-medium text-white p-2 rounded mt-4"
+                    onClick={handleRetake}
+                  />
+                  <Button
+                    text="Back to home"
+                    style="text-[#996CF1] font-medium border-2 border-[#996CF1] p-2 rounded mt-4"
+                    onClick={() => navigate("/")}
+                  />
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      )}
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default QuizTaking
+export default QuizTaking;
