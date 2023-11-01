217887da25901d94f5bcd4e3e1bc2d1710a90784
kirubel <kirato747@gmail.com>
2023-11-01 22:26:21 +0300

    Improve quiz creation: Add input validation and success message

--- Online Quiz Maker (Level 2 Task 2)/onlinequizmaker(frontend)/src/pages/quiz/CreateQuiz.js
+++ Online Quiz Maker (Level 2 Task 2)/onlinequizmaker(frontend)/src/pages/quiz/CreateQuiz.js
@@ -1,8 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import api from "../../utils/api";
 import Button from "../../components/common/Button";
+import { Result, Select } from "antd";
+import { useNavigate } from "react-router-dom";
+import {notification,Form,Input,message } from 'antd';
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+const { Option } = Select;
 
 function CreateQuiz() {
+  const navigate=useNavigate()
+  const [form]= Form.useForm()
+  const [formInteracted,setFormInteracted]=useState(false)
   const [question, setQuestion] = useState({
     category: "",
     title: "",
@@ -18,6 +27,7 @@ function CreateQuiz() {
   const [selectedQuizIndex, setSelectedQuizIndex] = useState(null);
   const [correctAnswerIndex, setCorrectAnswerIndex] = useState(0);
   const [questions, setQuestions] = useState([]);
+  const[showModal,setShowModal]=useState(false)
   // const[editView,setEditView]=useState(false)
   const author=localStorage.getItem("username")
   const authorID=localStorage.getItem("userID")
@@ -51,7 +61,7 @@ function CreateQuiz() {
     const correctAnswer = options[correctAnswerIndex];
     console.log(question,"************** at add or update")
 
-    if (selectedQuizIndex !== null) {
+    if (selectedQuizIndex !== null && options.filter(value => value === "").length<=2) {
       // We are in edit mode, update the existing question
       const updatedQuestions = [...questions];
       const selectedQuestion = updatedQuestions[selectedQuizIndex];
@@ -65,13 +75,15 @@ function CreateQuiz() {
       setOptions(["", "", "", ""]);
       setCorrectAnswer("");
       setSelectedQuizIndex(null);
-    } else {
+    } else if(selectedQuizIndex===null&&questionText!=="" && options.filter(value => value === "").length<=2) {
       // We are not in edit mode, add a new question
       const shortID = generateShortID(12);
+      const filteredOptions=options.filter(value => value!=="").map(value => value)
+      console.log(filteredOptions,"filteeda $$$$$$$$$################################$$$")
       const newQuestion = {
         shortID,
         questionText,
-        options,
+        options:filteredOptions,
         correctAnswer,
       };
       setQuestions([...questions, newQuestion]);
@@ -80,6 +92,17 @@ function CreateQuiz() {
       setQuestionText("");
       setOptions(["", "", "", ""]);
       setCorrectAnswer("");
+    }else{
+      toast.warn("A Question and at least 2 choices required",{
+        position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      })
     }
   };
 
@@ -94,26 +117,41 @@ function CreateQuiz() {
   // console.log(questions,"**************")
   const handleSubmit = async (e) => {
     e.preventDefault();
+    console.log("in here at least")
     console.log(question,"************** at submit")
-
-    const finishedQuestion = {
-      quizzes: [{ ...question, questions}],
-    };
+if(questions.length>0 &&question.title!==""&&question.description!==""&&question.category!==""){
+    const finishedQuestion = {...question, questions}
+    
     try{
     await  api.post("/quiz/",finishedQuestion)
+    form.resetFields()
+    setQuestions([])
+    setShowModal(true)
+   setQuestion({
+      category: "",
+      title: "",
+      description: "",
+    });
+
     }catch(err){
       console.log(err)
     }
-    console.log(finishedQuestion,"*************finished question")
+    console.log(finishedQuestion,"*************finished question")}
+    else{
+toast.warn("Missing field",{
+  position: "top-right",
+autoClose: 3000,
+hideProgressBar: false,
+closeOnClick: true,
+pauseOnHover: true,
+draggable: true,
+progress: undefined,
+theme: "light",
+})
+
+      
+    }
   };
-  // const handleAnswerSelection=(id,selectedAnswer)=>{
-  // setSelectedCorrectAnswer((prevSelectedAnser)=>({
-  //   ...prevSelectedAnser,
-  //   [id]:selectedAnswer
-  // }))
-  // setCorrectAnswer(selectedCorrectAnswer[id])
-  // console.log(selectedCorrectAnswer,"selected answer")
-  // }
   function generateShortID(length) {
     const characters = '0123456789abcdef';
     let shortID = '';
@@ -144,94 +182,122 @@ function CreateQuiz() {
     updatedQuestions.splice(index, 1);
     setQuestions(updatedQuestions);
   };
-
+console.log(question,"******* question")
   return (
+    <>
+ <ToastContainer
+ position="top-right"
+ limit={5}
+ autoClose={3000}
+ hideProgressBar={false}
+ newestOnTop={true}
+ closeOnClick
+ rtl={false}
+ pauseOnFocusLoss
+ draggable
+ pauseOnHover
+ theme="light"
+ />
+
     <div className="flex flex-wrap justify-around  px-4 pt-24 pb-6 bg-white rounded shadow">
       <div className="md:w-1/3">
       <h2 className="text-xl font-semibold">Quiz Question</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-4">
-          <label
-            htmlFor="questionTitle"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Quiz Title
-          </label>
-          <input
-            type="text"
-            id="questionTitle"
-            className="mt-1 p-2 w-full rounded border border-gray-300"
-            value={question.title}
-            onChange={(e) =>
-              setQuestion({ ...question, title: e.target.value })
-              
-            }
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            htmlFor="questionDescription"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Quiz Description
-          </label>
-          <input
-            type="text"
-            id="questionDescription"
-            className="mt-1 p-2 w-full rounded border border-gray-300"
-            value={question.description}
-            onChange={(e) =>
-              setQuestion({ ...question, description: e.target.value })
-            }
-          />
-        </div>
+      <Form 
+      form={form}
+      name="Addquiz"
+      onSubmit={handleSubmit}
+      >
+        <Form.Item
+        name="title"
+        label={ <p className="text-base font-[500] font-sans">Quiz Title</p> }
+        labelCol={{ span: 24 }} // Full width for the label
+    wrapperCol={{ span: 24 }} // Full width for the input
+        rules={[{
+          required:true,
+          message:"Quiz title is required"
+        }]}
+        >
+<Input type="text" onChange={
+       (e)=>       setQuestion({ ...question, title: e.target.value })
+
+}
+       className="h-[42px] text-lg"
+placeholder="Quiz title"/>
+        </Form.Item>
+        <Form.Item
+        name="description"
+        label={ <p className="text-base font-[500] font-sans">Quiz Description</p> }
+        labelCol={{ span: 24 }} // Full width for the label
+        wrapperCol={{ span: 24 }} // Full width for the input
+        rules={[{
+          required:true,
+          message:"Quiz description is required"
+        }]}
+        >
+<Input type="text" onChange={(e)=>setQuestion({...question, description: e.target.value})}
+       className="h-[42px] text-lg"
+placeholder="Quiz descrpition"/>
+        </Form.Item>
+        <Form.Item 
+        name="category"
+        label={ <p className="text-base font-[500] font-sans">Quiz Category</p> }
+        labelAlign="top" // Place the label on top
+        labelCol={{ span: 24 }} // Full width for the label
+    wrapperCol={{ span: 24 }} // Full width for the input
+    
+        rules={[
+          {
+          required:true,
+          message:"Quiz category is required"
+        }
+      ]}
+        >
+        <Select 
+       className="h-[42px] text-lg"
+        placeholder="Select a category or add a custom one"
+        onChange={(value) =>
+          setQuestion({ ...question, category: value })
+        }
+        >
+    <Option value="Chemistry">Chemistry</Option>
+    <Option value="Mathematics">Mathematics</Option>
+    <Option value="Physics">Physics</Option>
+    <Option value="Programming">Programming</Option>
+    <Option value="Biology">Biology</Option>
+    <Option value="History">History</Option>
+    <Option value="Geography">Geography</Option>
+    <Option value="Literature">Literature</Option>
+    <Option value="General Knowledge">General Knowledge</Option>
+    <Option value="Custom">Custom</Option>
+  </Select>
+        </Form.Item>
+      
         <div className="mb-4">
           <label
             htmlFor="questionText"
             className="block text-sm font-medium text-gray-700"
           >
-            Quiz Category
-          </label>
-          <select
-            type="text"
-            id="questionCategory"
-            className="mt-1 p-2 w-full rounded border border-gray-300"
-            value={question.category}
-            onChange={(e) =>
-              setQuestion({ ...question, category: e.target.value })
-            }
-          >
-            <option value="Chemistry">Chemistry</option>
-            <option value="Mathematics">Mathematics</option>
-            <option value="Physics">Physics</option>
-            <option value="Programming">Programming</option>
-
-          </select>
-        </div>
-
-        <div className="mb-4">
-          <label
-            htmlFor="questionText"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Question Text
+            Question
           </label>
           <input
             type="text"
             id="questionText"
-            className="mt-1 p-2 w-full rounded border border-gray-300"
+            className="mt-1 p-2 w-full rounded border border-gray-300 hover:border hover:ease-in  hover:border-[#4096FF] focus:outline  focus:outline-[#4096FF] focs:border-"
             value={questionText}
             onChange={(e) => setQuestionText(e.target.value)}
           />
         </div>
-        <label htmlFor="">Choices</label>
+        <label htmlFor="" className="font-medium">Choices</label>
         {options.map((option, index) => (
+
           <div key={index} className="flex space-x-2">
             <input
               type="text"
-              className="p-2 w-full rounded border border-gray-300"
+              className={`p-2 my-2 w-full rounded 
+              ${options.length<2&&formInteracted?"border border-[#FF7875]":"border border-gray-300 "} `}
               value={option}
               onChange={(e) => handleOptionChange(index, e.target.value)}
+              onBlur={()=>{setFormInteracted(true)}}
             />
            {index>1&& <button
               type="button"
@@ -242,10 +308,12 @@ function CreateQuiz() {
             </button>}
           </div>
         ))}
+       {formInteracted&&options.length<2&& <p className="text-red-600">Choices are required</p>}
        {options.length<=4&& <button
           type="button"
           className="text-blue-600 hover:text-blue-800"
           onClick={addOption}
+          disabled={options.length<2}
         >
           Add Option
         </button>}
@@ -276,11 +344,12 @@ function CreateQuiz() {
         </button>
         <button
           type="submit"
+          onClick={handleSubmit}
           className="bg-blue-500 ml-4 text-white p-2 rounded hover:bg-blue-600"
         >
           Submit quiz
         </button>
-      </form>
+      </Form>
       </div>
       <div>
         <h2 className="text-xl font-semibold">Your added Questions will appear here</h2>
@@ -304,7 +373,7 @@ function CreateQuiz() {
               </p>
               {questionItems.options.map((option, optionIndex) => (
                 <div className="flex flex-col" key={questionItems.shortID + optionIndex}>
-                  <label className="p-2" htmlFor="choice">
+                 {option!==""&& <label className="p-2" htmlFor="choice">
                     <input
                       type="radio"
                       className="mr-2"
@@ -314,7 +383,7 @@ function CreateQuiz() {
                       checked={selectedCorrectAnswer[questionItems.shortID] === option}
                     />
                     {option}
-                  </label>
+                  </label>}
                 </div>
               ))}
               <p>Correct Answer : {selectedCorrectAnswer[questionItems.shortID]}</p>
@@ -322,8 +391,36 @@ function CreateQuiz() {
           ))}
         </div>
       </div>
+      {showModal && (
+          <div className="fixed inset-0 flex items-center justify-center z-50">
+            <div className="bg-white flex items-center justify-center drop-shadow-lg shadow shadow-gray-400 rounded-lg py-10 px-24">
+              <div className="modal-content flex flex-col items-center justify-center">
+                <h2 className="md:text-2xl text-xl font-bold">Submitted Successfully</h2>
+                <Result
+    status="success"
+    title="Do you want to add another quiz or go back home?"
+    subTitle=""
+    extra={[
+      <Button
+       text="Add another quiz"
+      onClick={()=>setShowModal(false)}
 
+        style="bg-[#996CF1] font-medium text-white p-2 rounded mt-4"
+        />,
+      <Button 
+      text="Back to home" 
+      onClick={()=>navigate('/')}
+
+      style="text-[#996CF1] font-medium border-2 border-[#996CF1] p-2 rounded mt-4"
+      />,
+    ]}
+  />
+                </div>
+                </div>
+                </div>
+)}
     </div>
+    </>
   );
 }
 
