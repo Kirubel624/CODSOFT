40a29fd5ce661c1e37b6396b46d2b738c96e99f7
Kirabyte <97957123+Kirubel624@users.noreply.github.com>
2023-11-03 22:25:04 +0300

    Update api.js
    Fix API URL bug

--- onlinequizmaker/onlinequizmakerfrontend/src/utils/api.js
+++ onlinequizmaker/onlinequizmakerfrontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://quiztime-wjxb.onrender.com/",
+  baseURL: "http://quiztime-wjxb.onrender.com",
 });
 
 export default api;
