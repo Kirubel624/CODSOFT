2968fe3a1a6cdeb6c77574234d3b8c063dc18b52
kirubel <kirato747@gmail.com>
2023-11-03 22:32:01 +0300

    Leaderboard modal fix

--- onlinequizmaker/onlinequizmakerfrontend/src/pages/quiz/LeaderBoard.js
+++ onlinequizmaker/onlinequizmakerfrontend/src/pages/quiz/LeaderBoard.js
@@ -112,7 +112,7 @@ const Leaderboard = () => {
             > */}
            {viewLeaderBoard&&   <div className="fixed inset-0 flex items-center justify-center z-50">
       <div className="modal-overlay absolute w-full h-full bg-gray-900 opacity-50" ></div>
-      <div className="modal-container bg-white w-1/2 md:max-w-[40rem] mx-auto rounded shadow-lg z-50 overflow-y-auto">
+      <div className="modal-container bg-white w-full md:max-w-[40rem] mx-auto rounded shadow-lg z-50 overflow-y-auto">
         
         <div className="modal-content py-6 text-left px-6">
 
